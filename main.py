1e3653b6b156ce3c7ea8c179aefb504ccbfbf4de
Amey Kusurkar <amey1000@gmail.com>
2020-11-07 21:26:08 +0000

    Add sigmoid function

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ nnfs.init()
 class Layer:
     def __init__(self, num_inputs, num_neurons):
         # Shape of weights is (inputs x neurons) to save doing a transpose
-        self.weights = 0.1 * np.random.randn(num_inputs, num_neurons)
+        self.weights = np.random.randn(num_inputs, num_neurons)
         self.biases = np.zeros((1, num_neurons))
 
     def __call__(self, inputs):
-        return relu(np.dot(inputs, self.weights) + self.biases)
+        return sigmoid(np.dot(inputs, self.weights) + self.biases)
 
 class Network:
     def __init__(self, layer_sizes):
@@ -24,6 +24,9 @@ class Network:
 
 def relu(x):
     return np.maximum(0, x)
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
 
 X, y = spiral_data(100, 3)
 
